238760bb36a93da075ab4b9400d90f175fbe0e67
Gongmeda <gongmeda@gmail.com>
2024-04-09 11:50:12 +0900

    refactor: HttpResponse로 응답 책임 이동

---
+++ src/main/java/model/HttpResponse.java
@@ -0,0 +1,70 @@
+package model;
+
+import org.springframework.http.HttpStatus;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Map;
+
+public class HttpResponse {
+
+    private String httpVersion = "HTTP/1.1";
+    private HttpStatus status = HttpStatus.OK;
+    private HttpHeaders headers = new HttpHeaders();
+    private byte[] body = null;
+
+    public HttpResponse() {}
+
+    public HttpResponse(HttpStatus status, HttpHeaders headers, byte[] body) {
+        this.status = status;
+        this.headers = headers;
+        this.body = body;
+    }
+
+    public String getHttpVersion() {
+        return httpVersion;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public HttpHeaders getHeaders() {
+        return headers;
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+
+    public void setHttpVersion(String httpVersion) {
+        this.httpVersion = httpVersion;
+    }
+
+    public void setBody(byte[] body) {
+        this.body = body;
+    }
+
+    public void respond(DataOutputStream dos) throws IOException {
+        dos.writeBytes(String.format("%s %d %s \r\n", httpVersion, status.value(), status.getReasonPhrase()));
+        writeHeaders(dos);
+        dos.writeBytes("\r\n");
+
+        dos.write(body, 0, body.length);
+        dos.flush();
+    }
+
+    private void writeHeaders(DataOutputStream dos) throws IOException {
+        if (body != null && body.length > 0) {
+            dos.writeBytes("Content-Length: " + body.length + "\r\n");
+        }
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            dos.writeBytes(entry.getKey() + ": " + entry.getValue() + "\r\n");
+        }
+    }
+}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,15 +2,21 @@ package webserver;
 
 import model.HttpHeaders;
 import model.HttpRequest;
+import model.HttpResponse;
 import model.StartLine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.accept.MappingMediaTypeFileExtensionResolver;
 import utils.FileIoUtils;
 import utils.IOUtils;
 
 import java.io.*;
 import java.net.Socket;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -32,12 +38,12 @@ public class RequestHandler implements Runnable {
 
             HttpRequest httpRequest = parseRequest(bufferedReader);
 
-            // 1. template (html)
-            if (handleTemplates(httpRequest, dos)) {
+            String path = httpRequest.getStartLine().getPath();
+
+            if (isFile(path)) {
+                handleFile(httpRequest, dos);
                 return;
             }
-
-            // 2. static (css...)
 
             // 3. etc
 
@@ -46,6 +52,21 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private boolean isFile(String path) {
+        String mime = java.net.URLConnection.guessContentTypeFromName(path);
+        return mime != null;
+    }
+
+    private void handleFile(HttpRequest httpRequest, DataOutputStream dos) throws IOException{
+        // 1. template (html)
+        if (handleFileResponse("templates", httpRequest, dos)) {
+            return;
+        }
+
+        // 2. static (css...)
+        handleFileResponse("static", httpRequest, dos);
     }
 
     private HttpRequest parseRequest(BufferedReader br) throws IOException {
@@ -76,45 +97,47 @@ public class RequestHandler implements Runnable {
         return null;
     }
 
-    private void handleRoot(DataOutputStream dos) {
+    private void handleRoot(DataOutputStream dos) throws IOException {
         byte[] body = "Hello World".getBytes();
-        response200Header(dos, body.length);
-        responseBody(dos, body);
+
+        HttpResponse response = new HttpResponse();
+        response.getHeaders().put("Content-Type", "text/html;charset=utf-8");
+        response.setBody(body);
+
+        response.respond(dos);
     }
 
-    private boolean handleTemplates(HttpRequest request, DataOutputStream dos) throws IOException {
+    private boolean handleFileResponse(String parentFolder, HttpRequest request, DataOutputStream dos) throws IOException {
         try {
-            String filePath = "templates" + request.getStartLine().getPath();
-            System.out.println(filePath);
+            String path = request.getStartLine().getPath();
+            String filePath = parentFolder + path;
+
+            HttpResponse response = new HttpResponse();
+
             byte[] body = FileIoUtils.loadFileFromClasspath(filePath);
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            response.setBody(body);
+            response.setStatus(HttpStatus.OK);
+            response.setHttpVersion(request.getStartLine().getHttpVersion());
+
+            String mime = parseMIME(filePath);
+            response.getHeaders().put("Content-Type", mime + ";charset=utf-8");
+
+            response.respond(dos);
+
             return true;
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         } catch (NullPointerException e) {
-            logger.info("file does not exist in 'template'");
+            logger.info(String.format("file does not exist in '%s'", parentFolder));
             return false;
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
+    private String parseMIME(String path) {
+        String mimeType = java.net.URLConnection.guessContentTypeFromName(path);
+        if (mimeType == null) {
+            mimeType = "application/octet-stream";
         }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+        return mimeType;
     }
 }
