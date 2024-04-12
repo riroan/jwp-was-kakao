48749bce0bb5ec6625c9eef072383c4acfacea05
riroan <riroan@naver.com>
2024-04-12 15:31:17 +0900

    refactor: 상속 제거

--- src/main/java/http/HttpQueryParams.java
+++ src/main/java/http/HttpQueryParams.java
@@ -1,12 +1,26 @@
 package http;
 
 import java.util.HashMap;
+import java.util.Map;
 
-public class HttpQueryParams extends HashMap<String, String> {
+public class HttpQueryParams {
     private static final String QUERY_STRING_DELIMITER = "&";
     private static final String KEY_VALUE_DELIMITER = "=";
     private static final int KEY_INDEX = 0;
     private static final int VALUE_INDEX = 1;
+    private final Map<String, String> queryParam;
+
+    public HttpQueryParams() {
+        this.queryParam = new HashMap<>();
+    }
+
+    public void put(String key, String value) {
+        queryParam.put(key, value);
+    }
+
+    public String get(String key) {
+        return queryParam.get(key);
+    }
 
     public static HttpQueryParams of(String path) {
         String[] paths;
