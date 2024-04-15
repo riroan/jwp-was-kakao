608313ad642b4d6c0904e33f7e1898095aef017b
riroan <riroan@naver.com>
2024-04-15 15:09:46 +0900

    refactor: 헤더 생성자 추가

--- src/main/java/http/HttpHeaders.java
+++ src/main/java/http/HttpHeaders.java
@@ -1,15 +1,30 @@
 package http;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class HttpHeaders extends HashMap<String, String> {
+public class HttpHeaders {
+    private static final String LINE_DELIMITER = "\n";
+    private static final String HEADER_DELIMITER = ": ";
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+
+    private final Map<String, String> headers;
+
+    public HttpHeaders() {
+        this(new HashMap<>());
+    }
+
+    public HttpHeaders (Map<String, String> headers) {
+        this.headers = headers;
+    }
 
     public static HttpHeaders of(String text) {
-        HttpHeaders headers = new HttpHeaders();
-        for (String line : text.split("\n")) {
-            String[] tokens = line.split(": ");
-            headers.put(tokens[0], tokens[1]);
-        }
-        return headers;
+        Map<String, String> headers = Arrays.stream(text.split(LINE_DELIMITER))
+                .map(line -> line.split(HEADER_DELIMITER))
+                .collect(Collectors.toMap(tokens -> tokens[KEY_INDEX], tokens -> tokens[VALUE_INDEX]));
+        return new HttpHeaders(headers);
     }
 }
